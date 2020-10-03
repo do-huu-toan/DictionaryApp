da8263a430f96c44789241b5cae898cf4b30b405
Do Huu Toan <dohuutoannb@gmail.com>
2020-10-04 01:56:44 +0700

    add EditPanel

--- src/Views/DictionaryApplication.java
+++ src/Views/DictionaryApplication.java
@@ -5,20 +5,12 @@
  */
 package Views;
 import controllers.*;
-import models.*;
-import javax.swing.DefaultListModel;
-import com.sun.speech.freetts.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-import java.net.URLEncoder;
-import java.util.ArrayList;
 /**
  *
  * @author DHT
